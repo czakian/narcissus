bd864c0803c440a833d003883e467b1d9272a6ba
Chris <czakian@indiana.edu>
2011-10-22 21:10:30 -0400

    testing wget in go

--- crawler.go
+++ crawler.go
@@ -1,7 +1,20 @@
 package main
 
 import (
+"exec"
 "fmt"
+"io/ioutil"
 )
+
+
+//testing wget in go
+func main() {
+  website := "http://www.google.com"
+  val := exec.Command("wget", website)
+  val.Output()
+  infile := "index.html"
+  contents,_ := ioutil.ReadFile(infile)
+  fmt.Println(string(contents))
+}
 
 
